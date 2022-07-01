f8a0fa967a2abafbcd3a7cc5a2aa78c783e49c92
Zack Fielding <zackfielding@gmail.com>
2022-07-01 11:43:21 -0600

    cleaned up and removed multi threading for now

--- firstHash.cpp
+++ firstHash.cpp
@@ -5,28 +5,31 @@
 #import <thread>
 #import <vector>
 
-// [] fix compiler error related to returning std::map<>
-// [] make sure both custom and std::map hashing works
-// [] adjust threading
-
 	// global var
 std::vector<double*>hashArrayVector; 
 
-double* customMap (int *array, const int &arraySize, int(*hashing)(const int &),
+double* customMap (int *array, const int &arraySize, 
+		int(*hashing)(const int &),
 		void(*keyIndex)(int, int, double *)){
 
 	const int hashArraySize {42}; //set to 42 for simplicity
 	static int currentVector {0};
 	hashArrayVector.push_back(new double [hashArraySize]);
+		//vec containing double ptrs to heap
 
+		// initialize all values to NAN
 	for(size_t n {0}; n < hashArraySize; ++n)
-		hashArrayVector.at(currentVector)[n] = std::numeric_limits<double>::quiet_NaN();
+		hashArrayVector.at(currentVector)[n] = 
+			std::numeric_limits<double>::quiet_NaN();
 
+		//i (hashing()) -> hash current value to get key
+		//ii (keyIndex())-> index the key into the double[] on heap 
 	for(size_t k {0}; k < arraySize; ++k)
-		keyIndex(hashing(array[k]), array[k], hashArrayVector.at(currentVector));  
+		keyIndex(hashing(array[k]), array[k], 
+				hashArrayVector.at(currentVector));  
 	
 	++currentVector;
-	return hashArrayVector.at(currentVector-1); // -1 to account for previous++	
+	return hashArrayVector.at(currentVector-1);
 }
 
 int hashing(const int &toHash){
@@ -39,7 +42,8 @@ int hashing(const int &toHash){
 	else if(key == 100)
 	   return (key % 100);
 	else{
-		std::cerr << "Error in key determination for " << toHash << std::endl;	
+		std::cerr << "Error in key determination for " 
+			<< toHash << std::endl;	
 		return -1; //throw error down the line
 	}
 }
@@ -71,20 +75,13 @@ bool findValue(int key, double* hashArray){
 		return false;
 }
 
-void customHashRun(double* customHashArray, int array1[], const int &array1size, 
-		int(*hashing)(const int &), void(*keyIndex)(int, int, double *)){
-
-	customHashArray = customMap(array1, array1size, hashing, keyIndex);
-	std::cout << "Custom hash func completed." << std::endl;	
-	return;
-}
-
-// compiler errror related to returning std::map<>
-std::map<int, int> standardMapRun(int array1[], int array1size){
+void generateMap(std::map<int, bool> &map, 
+		int* array, const int& arraySize){
 	
-	std::map<int, int> standardMap(array1[0], array1[array1size-1]);
-	std::cout << "Standard map completed." << std::endl;
-	return standardMap;
+	for(size_t i {0}; i < arraySize; ++i)
+		map.insert({array[i], true});
+
+	return;	
 }
 
 int main(){
@@ -92,17 +89,14 @@ int main(){
 	int array1[] {1,2,3,4,5}, array2[] {0,2,6,8,9}, array3[5]{0};
 	const int array1size {5};
 
-	double *customHashArray {nullptr};
-	std::map<int, int> mapRecieve;
+	std::map<int, bool> map;
+	generateMap(map, array1, array1size); // generate ordered map	
 
-	std::thread customThread (customHashRun, customHashArray, array1, array1size,
-			hashing, keyIndex);
-	mapRecieve = standardMapRun(array1, array1size);	
-
-	customThread.join(); // wait for thread 2 to finish
+	double* myMap {nullptr};
+	myMap = customMap(array1, array1size, hashing, keyIndex);	
 
 	// free heap
-	for(double* ptr : hashArrayVector)
+	for(double* &ptr : hashArrayVector)
 		delete [] ptr;
 
 	return 0;
